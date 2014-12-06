6e382529f8d054b33bb047bb00ae061614f075a3
Simon Lawrence <simonl2002@gmail.com>
2014-12-06 16:25:07 -0500

    Tweet fetching first pass

--- cljsrc/bushtweets/project.clj
+++ cljsrc/bushtweets/project.clj
@@ -1,9 +1,11 @@
-(defproject bushtweets "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bushtweets "0.1.0"
+  :description "Tweet collection utility"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.5"]
+                 [com.twitter/hbc-core "2.2.0"]]
   :main ^:skip-aot bushtweets.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- cljsrc/bushtweets/src/bushtweets/core.clj
+++ cljsrc/bushtweets/src/bushtweets/core.clj
@@ -1,7 +1,54 @@
 (ns bushtweets.core
+  (:require [clojure.data.json :as json])
+  (:import (java.util ArrayList)
+           (java.util.concurrent LinkedBlockingQueue)
+           (com.twitter.hbc ClientBuilder)
+           (com.twitter.hbc.core Client Constants)
+           (com.twitter.hbc.core.endpoint StatusesFilterEndpoint)
+           (com.twitter.hbc.core.processor StringDelimitedProcessor)
+           (com.twitter.hbc.httpclient.auth Authentication OAuth1))
   (:gen-class))
 
+(def consumer-key "zryJcfij4Es6pdAOTb9LUd6EW")
+(def consumer-secret "cxxlYARbOgS08sOPdm2NCfn7MrGFniYAix472iYLENiAemFd0q")
+(def access-token "14400922-8yDq3k5fxqJD6hVLukcdCo8tNSqloElNjtY4FWUUI")
+(def access-token-secret "F1EVDkFyNmrj9mMU7TXTQ7bT32t1Km2AcZ9XOOR8MN7Ar")
+
+(defn create-queue [size]
+  (LinkedBlockingQueue. size))
+
+(defn create-endpoint [terms]
+  (-> (StatusesFilterEndpoint.)
+    (.trackTerms (ArrayList. terms))))
+
+(defn create-auth [consumer-key consumer-secret token secret]
+  (OAuth1. consumer-key consumer-secret token secret))
+
+(defn create-client [endpoint auth queue]
+  (-> (ClientBuilder.)
+    (.hosts Constants/STREAM_HOST)
+    (.endpoint endpoint)
+    (.authentication auth)
+    (.processor (StringDelimitedProcessor. queue))
+    (.build)))
+
+(defn run [terms queue-size]
+  (let [queue (create-queue queue-size)
+        endpoint (create-endpoint terms)
+        auth (create-auth consumer-key consumer-secret access-token access-token-secret)
+        client (create-client endpoint auth queue)]
+    (println "Starting client")
+    (.connect client)
+    (while (not (.isDone client))
+      (println "fetching message")
+      (let [msg-json (.take queue)
+            msg (json/read-str msg-json)
+            tweet (get msg "text")]
+        (println tweet)))
+    (println "shutting down client")
+    (.shutdown client)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "run run run"
   [& args]
-  (println "Hello, World!"))
+  (run ["bushwick" "twitter"] 10000))
